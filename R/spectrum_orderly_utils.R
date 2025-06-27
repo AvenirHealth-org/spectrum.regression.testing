e045ff20269a638b70ed6a7d6427f179c60c6b14
Rob <39248272+r-ash@users.noreply.github.com>
2025-06-27 16:50:52 +0100

    Improve formatting of orderly data captures

--- R/spectrum_orderly_utils.R
+++ R/spectrum_orderly_utils.R
@@ -10,12 +10,25 @@ list_spectrum_versions <- function(orderly_root) {
 list_orderly_versions <- function(orderly_root, spectrum_version, leapfrog) {
   task_name <- get_golem_config("orderly_task_name")
   ## TODO: Add leapfrog run parameter to this
-  orderly2::orderly_search(
-    "parameter:spectrum_version == this:spectrum_version",
+  ids <- orderly2::orderly_search(
+    "parameter:spectrum_version == this:spectrum_version && parameter:run_leapfrog == this:leapfrog",
     name = task_name,
     parameters = list(spectrum_version = spectrum_version,
                       leapfrog = leapfrog),
     root = orderly_root)
+
+  display_name <- vapply(ids, function(id) {
+    meta <- orderly2::orderly_metadata(id, root = orderly_root)
+    display_date <- lubridate::ymd_hms(lubridate::round_date(meta$time$end, unit = "second"))
+    git_hash <- meta$custom$orderly$description$custom$spectrum_git_hash
+    orderly_id <- strsplit(id, "-")[[1]][3]
+    if (is.null(meta$custom$orderly$description$custom$spectrum_git_hash)) {
+      sprintf("%s %s", display_date, orderly_id)
+    } else {
+      sprintf("%s [%s] %s", display_date, git_hash, orderly_id)
+    }
+  }, character(1))
+  setNames(ids, display_name)
 }
 
 get_orderly_file_paths <- function(orderly_root, orderly_id) {
